20c7714e11af696877d49d48b2c72cab1916c9e5
Aykut Efe Çağlayan <aykutefecaglayan@gmail.com>
2024-03-16 13:35:08 +0300

    Palindrom programı eklendi

---
+++ src/Palindrom.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Palindrom {
+    static boolean isPalindrom(int number) {
+        int temp = number , reverseNumber = 0 , lastNumber ;
+
+        while (temp != 0 ){
+
+            lastNumber = temp % 10 ;
+            reverseNumber = (reverseNumber * 10) + lastNumber ;
+            temp /= 10 ;
+
+        }
+        if (reverseNumber == number)
+            return true ;
+        else
+            return false ;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.print("Kontrol Etmek istediğiniz sayıyı giriniz : ");
+        Scanner input = new Scanner(System.in);
+        int x ;
+        x = input.nextInt();
+        System.out.println(isPalindrom(x));
+
+    }
+}
