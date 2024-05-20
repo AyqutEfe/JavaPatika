1c81d15852b1be93cb2e79376cc8cbe6e3f06518
Aykut Efe Çağlayan <aykutefecaglayan@gmail.com>
2024-05-20 15:29:56 +0300

    düzenlendi

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
+
 public class Palindrom {
     static boolean isPalindrom(int number) {
-        int temp = number , reverseNumber = 0 , lastNumber ;
+        int temp = number, reverseNumber = 0, lastNumber;
 
-        while (temp != 0 ){
+        while (temp != 0) {
 
-            lastNumber = temp % 10 ;
-            reverseNumber = (reverseNumber * 10) + lastNumber ;
-            temp /= 10 ;
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
 
         }
-        if (reverseNumber == number)
-            return true ;
-        else
-            return false ;
+        return reverseNumber == number;
     }
 
 
@@ -21,8 +19,7 @@ public class Palindrom {
 
         System.out.print("Kontrol Etmek istediğiniz sayıyı giriniz : ");
         Scanner input = new Scanner(System.in);
-        int x ;
-        x = input.nextInt();
+        int x = input.nextInt();
         System.out.println(isPalindrom(x));
 
     }
